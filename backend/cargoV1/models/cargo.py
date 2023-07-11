cade1e0cd20002dd77384f1a7e10e1b9077df2ed
ChrisShadow <chrisdanielbenee@gmail.com>
2023-07-11 12:03:38 -0400

    initial docs

--- backend/cargoV1/models/cargo.py
+++ backend/cargoV1/models/cargo.py
@@ -1,3 +1,14 @@
+#region Doc
+#Inheritance: Adoption of properties from the parent class into the child class.
+#Polymorphism: Creation of many forms from one form.
+#Abstraction: Displaying the necessary data and hiding unnecessary data.
+#Encapsulation: Securing the info of the class.
+"classes on Python: https://docs.python.org/3/tutorial/classes.html"
+"https://www.geeksforgeeks.org/python-classes-and-objects/"
+"https://realpython.com/python-namespaces-scope/#:~:text=the%20next%20level.-,Namespaces%20in%20Python,values%20are%20the%20objects%20themselves."
+"https://www.askpython.com/python/oops/class-instance-attributes#:~:text=You%20can%20access%20the%20attributes,would%20use%20the%20expression%20myClass."
+#endregion
+
 class Cargo():
     def __init__(self) -> None:
         pass
