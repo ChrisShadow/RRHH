df6be8884bb0f02a8ea751d6388a7fad85834687
ChrisShadow <chrisdanielbenee@gmail.com>
2023-07-26 12:37:19 -0400

    Classes actividades y divisa

--- backend/cargoV1/models/actividades.py
+++ backend/cargoV1/models/actividades.py
@@ -1,6 +1,49 @@
-class Actividades():
-    def __init__(self) -> None:
-        pass
+import json
 
-    def show_data():
-        return
+class Actividades():
+    #region constructor
+    def __init__(self,nombre, nivel):
+        self.nombre=nombre
+        self.nivel=nivel
+    #endregion
+
+    #region getter
+    def get_nombre(self):
+        return self.nombre
+    
+    def get_nivel(self):
+        return self.nivel
+    #endregion
+
+    #region setter
+    def set_nombre(self,new_name):
+        self.nombre=new_name
+
+    def set_nivel(self, new_level):
+        self.nivel=new_level
+    #endregion
+
+    #region methods
+
+    """It creates an Actividades object from a data string and uses the show_data() method to display the 
+    attributes of the created object. Class methods must have cls as the first parameter, which represents 
+    the class itself, so that they can access the class attributes and methods correctly. show_data() as a 
+    non-class method, but an instance method as we do not need to access the class attributes. We simply use 
+    it to show the instance attributes of the Actividades object."""
+    @classmethod
+    def crate_from_string(cls,data_string):
+        nombre, nivel=data_string.split(":")
+        return cls(nombre.strip(),nivel.strip())
+
+    def show_data(self):
+        response={
+            "Nombre de la responsabilidad: ": self.get_nombre(),
+            "Prioridad del cargo(Nivel 1: baja, Nivel 10: alta)": self.get_nivel()
+        }
+        json_response=json.dumps(response)
+        return json_response
+    #endregion
+
+    
+
+    
--- backend/cargoV1/models/divisa.py
+++ backend/cargoV1/models/divisa.py
@@ -1,6 +1,58 @@
-class Divisa():
-    def __init__(self) -> None:
-        pass
+import json
 
-    def show_data():
-        return
+class Divisa():
+    #region constructor
+    def __init__(self,simbolo,descripcion_moneda,valor,valor_mostrar):
+        self.simbolo=simbolo
+        self.descripcion_moneda=descripcion_moneda
+        self.valor=valor
+        self.valor_mostrar=None #It will update in the setter
+    #endregion
+
+    #region getter
+    def get_simbolo(self):
+        return self.simbolo
+    
+    def get_descripcion_moneda(self):
+        return self.descripcion_moneda
+    
+    def get_valor(self):
+        return self.valor
+    
+    def get_valor_mostrar(self):
+        return self.valor_mostrar
+    #endregion
+
+    #region setter
+    def set_simbolo(self, new_symbol):
+        self.simbolo=new_symbol
+
+    def set_descripcion_moneda(self, new_description_currency):
+        self.descripcion_moneda=new_description_currency
+    
+    def set_valor(self, new_value):
+        self.valor=new_value
+    
+    def set_valor_mostrar(self, simbolo ,valor):
+        self.valor_mostrar=(f"{simbolo}{valor:,.0f}" if self.descripcion_moneda in ["Guarani","Guaranies"]
+                            else f"{simbolo}{valor:,.2f}")
+    
+    #endregion
+
+    #region methods
+    @classmethod
+    def crate_from_string(cls,data_string):
+        simbolo, descripcion_moneda,valor,valor_mostrar=data_string.split(",")
+        return cls(simbolo.strip(),descripcion_moneda.strip(),valor.strip(),valor_mostrar.strip())
+    
+    def show_data(self):
+        response={
+            "Descripción: ": self.get_descripcion_moneda(),
+            "Monto: ": self.get_valor_mostrar()
+        }
+        json_response=json.dumps(response)
+        return json_response
+    #endregion
+    
+
+    
