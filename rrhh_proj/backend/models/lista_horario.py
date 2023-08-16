1598a39fd49887211c4567245898a3a386da242c
ChrisShadow <chrisdanielbenee@gmail.com>
2023-08-16 17:56:16 -0400

    listahorario: lugarTrabajo+Horarios, lógica con chatGpt

--- rrhh_proj/backend/models/lista_horario.py
+++ rrhh_proj/backend/models/lista_horario.py
@@ -1,12 +1,13 @@
 class ListaHorario():
     #Análisis de la lógica para implementar la integración entre lugarTrabajo:Horarios como colección, chatGpt
     #region construct
-    def __init__(self,test) -> None:
+    def __init__(self,test):
         self.test=test
     #endregion
 
     #region getter
-
+    def get_test(self):
+        return self.test
     #endregion
 
     #region setter
