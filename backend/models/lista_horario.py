c451456f5f439cc9940f08d527b8bd0d87d2d54a
ChrisShadow <chrisdanielbenee@gmail.com>
2023-09-03 21:40:56 -0400

    updating listahorario

--- backend/models/lista_horario.py
+++ backend/models/lista_horario.py
@@ -1,6 +1,23 @@
 class ListaHorario():
-    def __init__(self) -> None:
-        pass
+    #Análisis de la lógica para implementar la integración entre lugarTrabajo:Horarios como colección, chatGpt
+    """This class integrates LugarTrabajo and Horario in order to handle them as a collection in which the key is a single LugarTrabajo and the  value
+    could be list of Horarios with differents days and its time"""
 
+    #region construct
+    def __init__(self):
+        self.listaHorario={}
+    #endregion
+
+    #region getter
+    def get_test(self):
+        return self.test
+    #endregion
+
+    #region setter
+
+    #endregion
+
+    #region methods
     def show_data():
         return
+    #endregion
