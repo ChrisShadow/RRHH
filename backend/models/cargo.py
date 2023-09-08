63c8c0647cdeeab1966390ed73723ca980f8870a
ChrisShadow <chrisdanielbenee@gmail.com>
2023-09-07 23:13:53 -0400

    updating branch: ListaHorario

--- backend/models/cargo.py
+++ backend/models/cargo.py
@@ -8,9 +8,10 @@ from relacion_Horiz import RelacionHorizontal
 import json
 
 
-# region Doc
 
-# Some definitions
+#region Doc
+
+#Some definitions
 """Class->a template.
 Attribute->A variable within a class.
 Method->A function within a class.
@@ -18,13 +19,13 @@ Object->A particular instance of a class.
 Constructor->Code that runs when an object is created.
 Inheritance->The ability to extend a class to make a new class."""
 
-# Fundamental elements
+#Fundamental elements
 """Inheritance: Adoption of properties from the parent class into the child class.
 Polymorphism: Creation of many forms from one form.
 Abstraction: Displaying the necessary data and hiding unnecessary data.
 Encapsulation: Securing the info of the class."""
 
-# Classes in Python
+#Classes in Python 
 """They are created with the keyword class, followed by the name of the class. Class attributes are defined after the class- 
 -name, and they are shared by all instances of the class. Individual instance attributes are defined after the class attributes, and-
 -they are unique to each instance. Method definitions are also placed after the class definition. Methods are functions that are-
@@ -40,15 +41,15 @@ Basic Decorations: @classmethod, @staticmethod
 Classes inside another classes: Inner Classes
 Functions inside another functions: nested methods"""
 
-# Class attributes:
+#Class attributes:
 """If attributes are constant and do not change between different instances of the Cargo class, it is appropriate to define them as class attributes.
 They are shared between all instances of the class, which can save memory and avoid redundancy if the values are the same for all instances."""
 
-# Instance attributes:
+#Instance attributes:
 """If attributes are specific to each instance of the Cargo class and can change from one instance to another, they must be defined as instance attributes.
 They are initialised when a new instance of the class is created and belong exclusively to that instance."""
 
-# Resources
+#Resources
 """classes on Python: https://docs.python.org/3/tutorial/classes.html"
 https://youtu.be/LwFnF9XoEfM
 https://realpython.com/lessons/managing-class-attributes/
@@ -60,137 +61,133 @@ Public and private classes: https://youtu.be/xY__sjI5yVU
 Public and Private members: https://youtu.be/js9EITv4HqM
 Passign members from one class to another one: https://youtu.be/iDc_VrawjqY"""
 
-# endregion
+#endregion
 
 
 class Cargo():
     "static variables: those which are not from other classes"
-    # region class attibutes
-    TIPOFUNCIONARIO = {1: 'Asalariado', 2: 'Tercerizado'}
-    PERIODOPAGO = {1: 'Jornal', 2: 'Semanal', 3: 'Quincenal', 4: 'Mensual'}
-    # endregion
+    #region class attibutes
+    TIPOFUNCIONARIO={1:'Asalariado',2:'Tercerizado'}
+    PERIODOPAGO={1:'Jornal',2:'Semanal',3:'Quincenal',4:'Mensual'}
+    #endregion
 
     """Constructor lets the class initialize the object's attributes and serves no other purpose"""
-    # region constructor
-
-    def __init__(self, name, relacion_v_arriba, relacion_v_abajo, relacion_horizontal, lista_actividad, lista_horario, divisa, indice_tipo_funcionario, pago_funcionario, porcentaje_comision,
+    #region constructor
+    def __init__(self,name,relacion_v_arriba,relacion_v_abajo,relacion_horizontal,lista_actividad,lista_horario,divisa,indice_tipo_funcionario,pago_funcionario,porcentaje_comision,
                  indice_periodo_pago):
-        self.name = name
-        self._relacion_v_arriba = relacion_v_arriba
-        self._relacion_v_abajo = relacion_v_abajo
-        self._relacion_horizontal = relacion_horizontal
-        self._lista_actividad = lista_actividad
-        self._lista_horario = lista_horario
-        self._divisa = divisa
-        self.indice_tipo_funcionario = indice_tipo_funcionario
-        self.pago_funcionario = pago_funcionario
-        self.porcentaje_comision = porcentaje_comision
-        self.indice_periodo_pago = indice_periodo_pago
-    # endregion
+        self.name=name
+        self._relacion_v_arriba=relacion_v_arriba
+        self._relacion_v_abajo=relacion_v_abajo
+        self._relacion_horizontal=relacion_horizontal
+        self._lista_actividad=lista_actividad #ListaActividades()
+        self._lista_horario=lista_horario #ListaHorario()
+        self._divisa=divisa #Divisa
+        self.indice_tipo_funcionario=indice_tipo_funcionario
+        self.pago_funcionario=pago_funcionario
+        self.porcentaje_comision=porcentaje_comision
+        self.indice_periodo_pago=indice_periodo_pago
+    #endregion
 
     "All get functions retrieve the instance attributes, individually"
-    # region getter
-
+    #region getter
     def get_name(self):
         return self.name
-
+    
     """Here, the method show_data in RelacionVArriba displays the attributes as a whole in itself is accessed. 
     Basically the same with the others classes."""
-
     def get_relacion_v_arriba(self):
         return RelacionVArriba.show_data(self._relacion_v_arriba)
-
+    
     def get_relacion_v_abajo(self):
-        return RelacionVAbajo.show_data(self._relacion_v_abajo)
-
+        return RelacionVAbajo.show_data(self._relacion_v_abajo) 
+    
     def get_relacion_horizontal(self):
-        return RelacionHorizontal.show_data(self._relacion_horizontal)
-
+        return RelacionHorizontal.show_data(self._relacion_horizontal) 
+    
     def get_lista_actividad(self):
         return ListaActividades.show_data(self._lista_actividad)
-
+    
     def get_lista_horario(self):
         return ListaHorario.show_data(self._lista_horario)
-
+    
     def get_divisa(self):
         return Divisa.show_data(self._divisa)
-
+    
     def get_pago_funcionario(self):
         return self.pago_funcionario
-
+    
     def get_porcentaje_comision(self):
         return self.porcentaje_comision
 
     "Additional method to get the indexes and then access the values in the tipo_funcionario dictionary"
-
     def get_indice_tipo_funcionario(self):
         return self.indice_tipo_funcionario
 
     "Here if the indices change, the values you obtain using get_indice_tipo_funcionario() will also change automatically."
-
     def get_tipo_funcionario(self):
         return Cargo.TIPOFUNCIONARIO[self.get_indice_tipo_funcionario()]
-
+    
     def get_indice_periodo_pago(self):
         return self.indice_periodo_pago
 
     def get_periodo_pago(self):
         return Cargo.PERIODOPAGO[self.get_indice_periodo_pago()]
-
-    # endregion
+    
+    #endregion
 
     "All set functions enable to change of the values of those attributes,individually"
-    # region setter
-
+    #region setter
     def set_name(self, new_name):
-        self.name = new_name
+        self.name=new_name
+    
+    def set_relacion_v_arriba(self,new_relacion_v_arriba):
+        self._relacion_v_arriba=new_relacion_v_arriba
 
-    def set_relacion_v_arriba(self, new_relacion_v_arriba):
-        self._relacion_v_arriba = new_relacion_v_arriba
+    def set_relacion_v_abajo(self,new_relacion_v_abajo):
+        self._relacion_v_abajo=new_relacion_v_abajo
 
-    def set_relacion_v_abajo(self, new_relacion_v_abajo):
-        self._relacion_v_abajo = new_relacion_v_abajo
+    def set_relacion_horizontal(self,new_relacion_horizontal):
+        self._relacion_horizontal=new_relacion_horizontal
 
-    def set_relacion_horizontal(self, new_relacion_horizontal):
-        self._relacion_horizontal = new_relacion_horizontal
+    def set_lista_actividad(self,new_lista_actividad):
+        self._lista_actividad=new_lista_actividad
 
-    def set_lista_actividad(self, new_lista_actividad):
-        self._lista_actividad = new_lista_actividad
+    def set_lista_horario(self,new_lista_horario):
+        self._lista_horario=new_lista_horario
 
-    def set_lista_horario(self, new_lista_horario):
-        self._lista_horario = new_lista_horario
-
-    def set_divisa(self, new_divisa):
-        self._divisa = new_divisa
-
+    def set_divisa(self,new_divisa):
+        self._divisa=new_divisa
+    
     def set_pago_funcionario(self, new_pago_funcionario):
-        self.pago_funcionario = new_pago_funcionario
-
-    def set_porcentaje_comision(self, new_porcentaje_comision):
-        self.porcentaje_comision = new_porcentaje_comision
-
-    def set_indice_tipo_funcionario(self, new_indice_tipo_funcionario):
-        self.indice_tipo_funcionario = new_indice_tipo_funcionario
+        self.pago_funcionario=new_pago_funcionario
+    
+    def set_porcentaje_comision(self,new_porcentaje_comision):
+        self.porcentaje_comision=new_porcentaje_comision
+    
+    def set_indice_tipo_funcionario(self,new_indice_tipo_funcionario):
+        self.indice_tipo_funcionario=new_indice_tipo_funcionario
 
     "It is not required because changes won't be necessary in the dict"
-    # def set_tipo_funcionario(self,new_tipo_funcionario):
+    #def set_tipo_funcionario(self,new_tipo_funcionario):
     #    for key, value in self.tipo_funcionario.items():
     #        if value==new_tipo_funcionario:
     #            self.indice_tipo_funcionario=key
     #            break
+    
+    def set_indice_periodo_pago(self,new_indice_periodo_pago):
+        self.indice_periodo_pago=new_indice_periodo_pago
 
-    def set_indice_periodo_pago(self, new_indice_periodo_pago):
-        self.indice_periodo_pago = new_indice_periodo_pago
-
-    # def set_indice_periodo_pago(self,new_indice_periodo_pago):
+    #def set_indice_periodo_pago(self,new_indice_periodo_pago):
     #    for key, value in self.indice_periodo_pago.items():
     #        if value==new_indice_periodo_pago:
     #            self.indice_periodo_pago=key
     #            break
 
-    # endregion
+    
+    
+    #endregion
 
-   # region class methods
+   #region class methods
     """Here, the dictionary tipo_funcionario is obtained directly using the class name, Cargo."
     Class-exclusive method, since tipo_funcionario is of class type. The same with periodo_pago"""
     @classmethod
@@ -202,30 +199,30 @@ class Cargo():
         return cls.PERIODOPAGO
 
     "Display the data as a whole transformed as a json object in order send to the view as a json object."
-
     def show_data(self):
+        
         "In order to determine: Tipo de funcionario y remuneración"
-        tipo_funcionario = (f"Tipo funcionario: {self.get_tipo_funcionario()} " +
-                            f"Salario: {self.get_pago_funcionario()}" if self.indice_tipo_funcionario == 1
-                            else f"Tipo funcionario: {self.get_tipo_funcionario()} " +
-                            f"Viático: {self.get_pago_funcionario()}"
-                            )
-
+        tipo_funcionario=(f"Tipo funcionario: {self.get_tipo_funcionario()} " + 
+                          f"Salario: {self.get_pago_funcionario()}" if self.indice_tipo_funcionario==1 
+                          else f"Tipo funcionario: {self.get_tipo_funcionario()} " + 
+                          f"Viático: {self.get_pago_funcionario()}"
+                          )
+        
         "The response will be a tuple containing the dictionary as the first element and the JSON string as the second element."
-        response = {
+        response= {
             "Nombre del cargo: ": self.get_name(),
-            "Relación vertical arriba: ": self.get_relacion_v_arriba(),
+            "Relación vertical arriba: ": self.get_relacion_v_arriba(), 
             "Relación vertical abajo: ": self.get_relacion_v_abajo(),
             "Relación horizontal: ": self.get_relacion_horizontal(),
-            "Lista de actividades: ": self.get_lista_actividad(),
+            "Lista de actividades: ":self.get_lista_actividad(),
             "Lista de horario: ": self.get_lista_horario(),
-            "Tipo de funcionario y remuneración: ": tipo_funcionario,
+            "Tipo de funcionario y remuneración: ":tipo_funcionario,
             "Porcentaje Comisión: ": self.get_porcentaje_comision(),
             "Moneda: ": self.get_divisa(),
             "Período Pago: ": self.get_periodo_pago()
-        }
+            }
 
         "dumps() of the json module takes the dictionary as input and returns a text string in JSON format"
-        json_response = json.dumps(response)
+        json_response=json.dumps(response)
         return json_response
-   # endregion
+   #endregion
--- backend/models/lista_horario.py
+++ backend/models/lista_horario.py
@@ -1,23 +1,70 @@
+from horario import Horario
+from lugar_trabajo import LugarTrabajo
+import json
+
 class ListaHorario():
-    #Análisis de la lógica para implementar la integración entre lugarTrabajo:Horarios como colección, chatGpt
+    
     """This class integrates LugarTrabajo and Horario in order to handle them as a collection in which the key is a single LugarTrabajo and the  value
     could be list of Horarios with differents days and its time"""
 
     #region construct
     def __init__(self):
-        self.listaHorario={}
+        self._listaHorario={}
     #endregion
 
     #region getter
-    def get_test(self):
-        return self.test
+    def get_lista_horario(self):
+        return self._listaHorario
     #endregion
 
     #region setter
-
+    def set_lista_horario(self, new_list):
+        self._listaHorario=new_list
     #endregion
 
     #region methods
-    def show_data():
-        return
+    """
+    We accept an instance of LugarTrabajo and a list of Horarios. 
+    We then use the extend method to add the Horarios to the existing list of Horarios associated with the LugarTrabajo.
+    """
+    @classmethod
+    def add_horario_lugar_trabajo(self,lugar_trabajo,horarios):
+        if lugar_trabajo not in self._listaHorario:
+            self._listaHorario[lugar_trabajo]=[]
+        self._listaHorario[lugar_trabajo].extend(horarios)
+
+    """
+    A specific Horario is added to an existing LugarTrabajo in the Horario list without deleting existing Horarios. 
+    Useful for expanding the list of Horarios  associated with a LugarTrabajo.
+    """
+    def add_horario_to_lugar_trabajo(self,lugar_trabajo,horario):
+        if lugar_trabajo in self._listaHorario:
+            self._listaHorario[lugar_trabajo].append(horario)
+
+    """"
+    It removes a specific Horario from a LugarTrabajo and also removes LugarTrabajo if it no longer has associated Horarios.
+    """
+    def remove_horario_lugar_trabajo(self, lugar_trabajo,horario):
+        if lugar_trabajo in self._listaHorario and horario  in self._listaHorario[lugar_trabajo]:
+            self._listaHorario[lugar_trabajo].remove(horario)
+            if not self._listaHorario[lugar_trabajo]:
+                del self._listaHorario[lugar_trabajo]
+    
+    #show_horarios_por_lugar
+    """
+    The key in the dictionary horarios_dict will be the combination of Empresa and Sucursal of LugarTrabajo. 
+    This will allow it to group Horarios under the unique combinations of Empresa and Sucursal instead of just the company name.
+    """
+    @classmethod
+    def show_data(self):
+        horarios_dict={}
+        for lugar,horarios in self._listaHorario.items():
+            lugar_combination=lugar.get_empresa_sucursal_combination()
+            horarios_dict[lugar_combination]= [horario.show_data() for horario in horarios]
+        return json.dumps(horarios_dict)
     #endregion
+
+
+   
+
+    
