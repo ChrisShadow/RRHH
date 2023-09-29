b797a1de4d8db9a814d90edf11cbbcd0a1aa61a2
ChrisShadow <chrisdanielbenee@gmail.com>
2023-09-29 17:30:09 -0400

    logica ListaActividad

--- backend/models/lista_actividades.py
+++ backend/models/lista_actividades.py
@@ -1,6 +1,31 @@
-class ListaActividades():
-    def __init__(self) -> None:
-        pass
+from actividades import Actividades
+import json
 
-    def show_data():
-        return
+
+class ListaActividades():
+
+    #region constructor
+    def __init__(self):
+        self.lista_actividad={}
+    #endregion
+
+    #region methods
+    def add_actividad(self, name,level):
+        actividad=Actividades(name,level)
+        self.lista_actividad[name]=actividad
+
+    def update_actividad(self, name, level):
+        if name in self.lista_actividad:
+            actividad=self.lista_actividad[name]
+            actividad.set_nivel(level)
+
+    def remove_actividad(self,name):
+        if name in self.lista_actividad:
+            del self.lista_actividad[name]
+
+    def show_data(self):
+        act_dict={}
+        for actividad in self.lista_actividad:
+            act_dict[actividad.get_nombre()]=actividad.get_nivel()
+        return json.dumps(act_dict)
+    #endregion
