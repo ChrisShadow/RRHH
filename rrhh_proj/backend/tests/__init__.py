98c70bd0522fd43574c49e0fefab67ee07f30b71
ChrisShadow <chrisdanielbenee@gmail.com>
2023-10-09 16:18:00 -0300

    test env, step1

---
+++ rrhh_proj/backend/tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+It initialises a package and is normally empty.
+Also, to perform initialisation actions when importing the package: 
+-Import Modules or Classes
+-Variable or Constant Configuration
+-Custom Initialisation (This can be useful for configuring things like registers, database connections, etc.)
+"""
