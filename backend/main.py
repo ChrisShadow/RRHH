406e5654b0b95a4bd383618e2fdc59c365218b0f
ChrisShadow <chrisdanielbenee@gmail.com>
2023-10-19 16:21:57 -0300

    checking main

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,11 @@ def main():
         # Create instances of the needed classes
 
         #Actividades: Creating an instance of it from a data string
-        activ1_data_string="Tarea1: Mala"
+        activ1_data_string="Tarea1: Alto"
         actividad1=Actividades.create_from_string(activ1_data_string)
+        print(f"Probando con {actividad1.get_nombre()}\n"+
+            f"Name:{actividad1.get_nombre()}\n"+
+            f"Level:{actividad1.get_nivel()}\n")
             
         # Display the instance data as a JSON string
         data_activ1_as_json=Actividades.show_data(actividad1)
@@ -23,7 +26,7 @@ def main():
         actividad1.set_nombre("Tarea 2")
         actividad1.set_nivel("Alto")
 
-        activ2_data_string="Tarea 3: Buena"
+        activ2_data_string="Tarea 3: Bajo"
         actividad2=Actividades.create_from_string(activ2_data_string)
         
             
@@ -54,6 +57,23 @@ def main():
         data_activs_as_json=lista_activs.show_data()
         print(f"Data as JSON format of ListaActividades\n"+
             f"{data_activs_as_json}")
+        
+        #Updating 
+        print(f"\nUpdating {actividad2.get_nombre()}")
+        #new_act=actividad2.set_nivel("Alto")
+        lista_activs.update_actividad("Tarea 5","Bajo")
+        data_activs_as_json=lista_activs.show_data()
+        print(f"Data as JSON format of ListaActividades\n"+
+            f"{data_activs_as_json}")
+
+        #Deleting Actividades from de collection
+        print(f"\nDeleting {actividad5.get_nombre()},{actividad2.get_nombre()} from the collection")
+        lista_activs.remove_actividades(actividad5,actividad2)
+        
+        data_activs_as_json=lista_activs.show_data()
+        print(f"Data as JSON format of ListaActividades\n"+
+            f"{data_activs_as_json}")
+        
     except ValueError as e:
          print(f"Error: {e}")
 
