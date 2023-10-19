65c4a403e3de65c4d2f2d1ab0f19ae58f097fe88
ChrisShadow <chrisdanielbenee@gmail.com>
2023-10-19 16:18:52 -0300

    checking list_act

--- backend/models/lista_actividades.py
+++ backend/models/lista_actividades.py
@@ -20,12 +20,21 @@ class ListaActividades():
 
     def update_actividad(self, name, level):
         if name in self.lista_actividad:
-            actividad=self.lista_actividad[name]
-            actividad.set_nivel(level)
+           self.lista_actividad[name].set_nivel(level)
+           """ actividad=self.lista_actividad[name]
+            actividad.set_nivel(level)"""
 
-    def remove_actividad(self,name):
-        if name in self.lista_actividad:
-            del self.lista_actividad[name]
+    def remove_actividades(self, *actividades):
+        for actividad in actividades:
+            for name, instance in list(self.lista_actividad.items()):
+                if instance == actividad:
+                    del self.lista_actividad[name]
+                    break
+
+    """def remove_actividad(self,*names):
+        for name in names:
+            if name in self.lista_actividad:
+                del self.lista_actividad[name]"""
 
     """def show_data(self):
         act_dict={}
@@ -39,5 +48,5 @@ class ListaActividades():
         for name,actividad in self.lista_actividad.items():
             act_dict[name]=actividad.get_nivel()
         return json.dumps(act_dict)
-    
+
     #endregion
