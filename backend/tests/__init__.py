fe156f22640ca65e0757f02b028d79c7abb6e0d1
ChrisShadow <chrisdanielbenee@gmail.com>
2023-12-22 17:05:50 -0300

    unnecessary directory

--- backend/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-It initialises a package and is normally empty.
-Also, to perform initialisation actions when importing the package: 
--Import Modules or Classes
--Variable or Constant Configuration
--Custom Initialisation (This can be useful for configuring things like registers, database connections, etc.)
-"""
