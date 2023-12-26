9741dc45444bcef1755740c32a4054aaa28cffe3
ChrisShadow <chrisdanielbenee@gmail.com>
2023-12-26 20:26:01 -0300

    comments, 2nd part

--- backend/models/divisa.py
+++ backend/models/divisa.py
@@ -3,6 +3,12 @@ import json
 
 class Divisa():
     # region constructor
+    """It defines a class called "divisa" with an initializer that takes ç
+    in parameters for the currency symbol, currency description, and currency 
+    value. It also has an optional parameter for the displayed value. If the 
+    displayed value is not provided, it is set using the currency symbol 
+    and value."""
+
     def __init__(self, simbolo, descripcion_moneda, valor, valor_mostrar=None):
         self.simbolo = simbolo
         self.descripcion_moneda = descripcion_moneda
@@ -17,6 +23,8 @@ class Divisa():
     # endregion
 
     # region getter
+    """These methods return the values of the corresponding attributes of the class."""
+
     def get_simbolo(self):
         return self.simbolo
 
@@ -31,6 +39,11 @@ class Divisa():
     # endregion
 
     # region setter
+    """This code defines a class Divisa with methods to set the symbol, 
+    currency description, and value. It also includes a method to evaluate 
+    the currency description and set the currency value to be displayed 
+    accordingly."""
+
     def set_simbolo(self, new_symbol):
         self.simbolo = new_symbol
 
@@ -40,7 +53,7 @@ class Divisa():
     def set_valor(self, new_value):
         self.valor = new_value
 
-    "Evaluates the description to define the symbol and the currency value."
+    "It evaluates the description to define the symbol and the currency value."
 
     def set_valor_mostrar(self, simbolo, valor):
         self.valor_mostrar = (f"{simbolo}{valor:,.0f}" if self.descripcion_moneda in ["Guarani", "Guaranies"]
@@ -49,13 +62,21 @@ class Divisa():
     # endregion
 
     # region methods
-
+    """This defines a class method called "create_from_string" which takes 
+    a data string as input. The data string is expected to be in the format 
+    "symbol, currency_description, value". The method splits the string using 
+    commas as the delimiter and assigns the split values to variables. It then 
+    returns an instance of the class with the stripped and converted values."""
     @classmethod
     def create_from_string(cls, data_string):
         simbolo, descripcion_moneda, valor, = data_string.split(
             ",")
         return cls(simbolo.strip(), descripcion_moneda.strip(), float(valor.strip()))
 
+    """It defines a class method called "show_data" that takes a "divisa" 
+    object as input. It retrieves the symbol, description, and value of the 
+    currency from the "divisa" object and creates a JSON response containing 
+    this information. The JSON response is then returned."""
     @classmethod
     def show_data(cls, divisa):
         response = {
